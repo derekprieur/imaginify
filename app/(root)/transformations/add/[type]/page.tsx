b20d41314bd245aa0d7bd8f99f85549bc54507aa
Derek Prieur <DPrieur123@gmail.com>
2024-03-06 08:05:24 -0500

    update

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,5 +1,15 @@
-const AddTransformationTypePage = () => {
-  return <div>AddTransformationTypePage</div>;
+import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { transformationTypes } from "@/constants";
+
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+  return (
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <TransformationForm />
+    </>
+  );
 };
 
 export default AddTransformationTypePage;
